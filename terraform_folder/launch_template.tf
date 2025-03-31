e84ac991982f2a060a7cb2baf4df465a1302961e
eedunoh <63975169+eedunoh@users.noreply.github.com>
2025-03-31 23:07:37 +0100

    added name tag to ec2 launch template

--- terraform_folder/launch_template.tf
+++ terraform_folder/launch_template.tf
@@ -25,6 +25,10 @@ resource "aws_launch_template" "static_file_app_lt" {
     }
 
     user_data = base64encode(file("user_data.sh"))
+
+    tags = {
+      Name = "Static_File_App-Server"
+    }
 }
 
 
